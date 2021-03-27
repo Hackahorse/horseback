8156ac0776fad42f43931ec25a1b388b309f7763
Alexey Shapoval <as@eubclab.com>
2021-03-27 10:03:08 +0200

    init

---
+++ src/main/java/com/hackahorse/backend/Horseback.java
@@ -0,0 +1,13 @@
+package com.hackahorse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Horseback {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Horseback.class, args);
+	}
+
+}
